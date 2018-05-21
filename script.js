8c3c51bf0d592d999bc6f6929ae0dde06be24cea
SSRafi <1430974@iub.edu.bd>
2018-05-21 04:50:04 +0000

    task3

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 /* JavaScript code here */
+//task 1
 console.log("External JS")
+//"task2"
+
+function sumArray(arr){
+    var sum=0;
+    arr.forEach(function(value, index){
+        sum+=value;
+        
+    });
+    return sum;
+}
+console.log(sumArray([1,2,3,4,5]));
+console.log(sumArray([1,20,-3,45]));
+// task3
+function checkEmail(emailString) {
+    var emailFormat=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var match= emailFormat.test(emailString);
+    return match;
+}
+console.log(checkEmail("john@smith.com"));
